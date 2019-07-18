4e4fac848e927a0a8eb96f05e736adbcf2be2218
mohanjaga <mj168jayam@gmail.com>
2019-07-18 13:23:16 +0530

    Updates

---
+++ precache-manifest.3fe982236f857997172a4de6fd4146e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73ed2d4cf28ba8434a48affedb879bf",
+    "url": "/personList/index.html"
+  },
+  {
+    "revision": "546f5f6f66f54cd78c0c",
+    "url": "/personList/static/css/main.97210639.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/personList/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "546f5f6f66f54cd78c0c",
+    "url": "/personList/static/js/main.8ebbf629.chunk.js"
+  },
+  {
+    "revision": "55b4f9817850a8771bf7",
+    "url": "/personList/static/js/runtime~main.69359877.js"
+  }
+]);
